e06be7b427c1165c32e462a91e6b476d045b6550
afsalahamada-ac9 <afsalahamad@abovecloud9.ai>
2025-04-25 15:43:02 +0530

    with error

--- review_file.go
+++ review_file.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 )
 
+const name = "hello"
+
 // this function prints hello world
 func hglloHandler(w http.ResponseWriter, r *http.Request) {
 	name := r.URL.Query().Get("name")
@@ -14,3 +16,5 @@ func main() {
 	http.HandleFunc("/hello", hglloHandler)
 	http.ListenAndServe(":12000", nil)
 }
+
+const name1 = "insert"
